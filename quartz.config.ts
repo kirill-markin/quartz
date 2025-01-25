ec8eb7afa5c86fabb332d249eaeeaf04398a20eb
Kirill Markin <markinkirill@gmail.com>
2025-01-25 21:28:13 +0100

    Remove emoji from page title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "👾 Kirill's notes",
+    pageTitle: "Kirill's notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
