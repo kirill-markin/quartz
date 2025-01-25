c3e8fc726eeb52e94607c4f4c8696c5e92575b5a
Kirill Markin <markinkirill@gmail.com>
2025-01-25 20:50:53 +0100

    Update site page title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Kirill Markin's Digital Garden",
+    pageTitle: "👾 Kirill's notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
