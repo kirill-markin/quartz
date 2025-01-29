6f0f81822d276bb32d987ff2842e59a70b56d65a
Kirill Markin <markinkirill@gmail.com>
2025-01-29 11:12:57 +0100

    feat: switch to ExplicitPublish filter for better content control

--- quartz.config.ts
+++ quartz.config.ts
@@ -81,7 +81,7 @@ const config: QuartzConfig = {
       Plugin.Latex({ renderEngine: "katex" }),
     ],
     filters: [
-      Plugin.RemoveDrafts(),
+      Plugin.ExplicitPublish(),
     ],
     emitters: [
       Plugin.AliasRedirects(),
